60ef95e6ccfa717dd01f07079d6ac21f34a123b3
Serhii Panov <suboxoneforhelp@gmail.com>
2026-08-07 10:06:40 +0300

    searchbar updated to use tailwind css, all components now use tailwind

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,4 +1,3 @@
-import css from "./SearchBox.module.css"
 
 interface SearchBoxProps {
   onSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -8,7 +7,7 @@ interface SearchBoxProps {
 export default function SearchBox(props: SearchBoxProps) {
   return (
           <input
-            className={css.input}
+            className="flex-1 max-w-[240px] px-1.5 py-2 text-base border border-[#ced4da] rounded-sm bg-white text-[#212529] focus:outline-none focus:ring-2 focus:ring-[#0d6efd] focus:border-[#0d6efd]"
             type="text"
             name="query"
             placeholder="Search notes"
