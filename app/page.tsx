c4ccbcb081e76fcc77861bc49c644732299a82cf
Serhii Panov <suboxoneforhelp@gmail.com>
2026-08-08 09:56:19 +0300

    added styles to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
 export default function Home() {
   return (
-    <main>
-  <div>
-    <h1>Welcome to NoteHub</h1>
-    <p>
-      NoteHub is a simple and efficient application designed for managing
+  <div className="container mx-auto px-4 py-8 bg-[#f0f0f0] rounded-2xl">
+    <h1 className="text-3xl font-bold mb-12 text-center">Welcome to Noted</h1>
+    <p className="mb-4 text-2xl">
+      Noted is a simple and efficient application designed for managing
       personal notes. It helps keep your thoughts organized and accessible
       in one place, whether you are at home or on the go.
     </p>
-    <p>
+    <p className="mb-4 text-2xl">
       The app provides a clean interface for writing, editing, and browsing
       notes. With support for keyword search and structured organization,
-      NoteHub offers a streamlined experience for anyone who values clarity
+      Noted offers a streamlined experience for anyone who values clarity
       and productivity.
     </p>
   </div>
-</main>
 
   );
 }
