b6c913f6355d89a7c72389076f16942b122ac5da
Serhii Panov <suboxoneforhelp@gmail.com>
2026-08-15 14:27:26 +0300

    Api url changed

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const RENDER_BACKEND_URL = 'https://noted-backend-h249.onrender.com';
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:8000',
+  baseURL: process.env.API_URL || RENDER_BACKEND_URL,
+  withCredentials: true,
 });
 const nextProxyServer = axios.create({
-  baseURL: baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || '/api',
   withCredentials: true,
 });
 
